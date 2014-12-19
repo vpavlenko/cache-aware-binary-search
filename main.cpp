84e95e095f232fd71ad4ad02bdc6491dfb7ae63d
Vitaly Pavlenko <cxielamiko@gmail.com>
2014-12-19 11:44:00 +0300

    before a break

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <chrono>
 #include <random>
+#include <string>
 
 using namespace std;
 
@@ -44,6 +45,44 @@ int binary_search_slow(int *array, int array_size, int value) {
     return array[left];
 }
 
+class FastBinarySearch {
+public:
+    FastBinarySearch(int *sorted_array, int sorted_array_size) :
+            sorted_array_(sorted_array), sorted_array_size_(sorted_array_size) {
+
+    }
+
+    int upper_bound(int query) const {
+
+    }
+
+private:
+    int *sorted_array_;
+    int sorted_array_size_;
+};
+
+class Measurer {
+public:
+    Measurer(string name, int num_iters) : name_(name), num_iters_(num_iters) {}
+
+    void start() {
+        start_ = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop() {
+        auto stop = std::chrono::high_resolution_clock::now();
+        auto delta = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start_).count();
+        assert((sizeof delta) == 8);
+        std::cout << name_ << ", " << num_iters_ << "iterations: " << delta << " ns";
+        std::cout << name_ << ": " << delta / (double)num_iters_ << " ns per iteration";
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point start_;
+    std::string name_;
+    int num_iters_;
+};
+
 int main() {
     cout << "get_sorted_array BEGIN\n";
     int *sorted_array = get_sorted_array(SORTED_ARRAY_SIZE);
@@ -54,12 +93,22 @@ int main() {
     int *slow_result = new int[NUM_ITERS];
     int *fast_result = new int[NUM_ITERS];
 
-    auto start = std::chrono::high_resolution_clock::now();
+    Measurer measurer_slow("slow", NUM_ITERS);
+    measurer_slow.start();
     for (int i = 0; i < NUM_ITERS; ++i) {
         slow_result[i] = binary_search_slow(sorted_array, SORTED_ARRAY_SIZE, query[i]);
     }
-    auto stop = std::chrono::high_resolution_clock::now();
-    std::cout << "slow, " << NUM_ITERS << " iterations: " << std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count() << " ns";
+    measurer_slow.stop();
+
+    Measurer measurer_fast("fast", NUM_ITERS);
+    FastBinarySearch fastBinarySearch(sorted_array, SORTED_ARRAY_SIZE);
+    measurer_fast.start();
+    for (int i = 0; i < NUM_ITERS; ++i) {
+        fast_result[i] = fastBinarySearch.upper_bound(query[i]);
+    }
+    measurer_fast.stop();
+
+    // TODO: results assertion
 
     return 0;
 }
