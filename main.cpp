a487f424192c5ccf757a22bec5ee1dfa8b8adc26
Vitaly Pavlenko <cxielamiko@gmail.com>
2014-12-19 12:00:28 +0300

    Before debug

--- main.cpp
+++ main.cpp
@@ -4,11 +4,13 @@
 #include <chrono>
 #include <random>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 const int SORTED_ARRAY_SIZE = 100 * 1000 * 1000;
 const int NUM_ITERS = 100 * 1000;
+const int L3_INT_ARRAY_SIZE = 100 * 1000;
 
 int *get_sorted_array(const int array_size) {
     int *result = new int[array_size];
@@ -33,32 +35,71 @@ int *get_random_array(int array_size, int rand_max) {
 }
 
 int binary_search_slow(int *array, int array_size, int value) {
-    int left = 0, right = array_size;
-    while (left + 1 < right) {
-        int mid = (left + right) >> 1;
-        if (array[mid] <= value) {
-            left = mid;
-        } else {
-            right = mid;
-        }
-    }
-    return array[left];
+
 }
 
-class FastBinarySearch {
+class SlowBinarySearch {
 public:
-    FastBinarySearch(int *sorted_array, int sorted_array_size) :
+    SlowBinarySearch() {}
+
+    SlowBinarySearch(int *sorted_array, int sorted_array_size) :
             sorted_array_(sorted_array), sorted_array_size_(sorted_array_size) {
 
     }
 
     int upper_bound(int query) const {
-
+        int left = 0, right = sorted_array_size_;
+        while (left + 1 < right) {
+            int mid = (left + right) >> 1;
+            if (sorted_array_[mid] <= query) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
     }
 
 private:
     int *sorted_array_;
     int sorted_array_size_;
+};
+
+class FastBinarySearch {
+public:
+    FastBinarySearch(int *sorted_array, int sorted_array_size) :
+            sorted_array_(sorted_array), sorted_array_size_(sorted_array_size) {
+        chunk_size_ = sorted_array_size / L3_INT_ARRAY_SIZE;
+        int inner_array_size = ceil((double)sorted_array_size / chunk_size_);
+        cout << "chunk_size: " << chunk_size_ << "\n";
+        cout << "inner_array_size: " << inner_array_size << "\n";
+
+        int *top_level_sorted_array = new int[inner_array_size];
+        for (int i = 0; i < inner_array_size; ++i) {
+            top_level_sorted_array[i] = sorted_array[i * chunk_size_];
+        }
+        topLevelBinarySearch_ = SlowBinarySearch(top_level_sorted_array, inner_array_size);
+    }
+
+    int upper_bound(int query) const {
+        int top_level_answer = topLevelBinarySearch_.upper_bound(query);
+        int left = top_level_answer * chunk_size_;
+        int right = min((top_level_answer + 1) * chunk_size_, sorted_array_size_);
+        while (left + 1 < right) {
+            int mid = (left + right) >> 1;
+            if (sorted_array_[mid] <= query) {
+                left = mid;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+private:
+    int *sorted_array_;
+    int sorted_array_size_, chunk_size_;
+    SlowBinarySearch topLevelBinarySearch_;
 };
 
 class Measurer {
@@ -83,6 +124,16 @@ private:
     int num_iters_;
 };
 
+void assertEquals(int *first_array, int *second_array, int array_size) {
+    for (int i = 0; i < array_size; ++i) {
+        if (first_array[i] != second_array[i]) {
+            throw std::runtime_error(string("difference at ") + to_string(i) + ": "
+                "first_array[i] = " + to_string(first_array[i]) + ", "
+                "second_array[i] = " + to_string(second_array[i]));
+        }
+    }
+}
+
 int main() {
     cout << "get_sorted_array BEGIN\n";
     int *sorted_array = get_sorted_array(SORTED_ARRAY_SIZE);
@@ -102,13 +153,19 @@ int main() {
 
     Measurer measurer_fast("fast", NUM_ITERS);
     FastBinarySearch fastBinarySearch(sorted_array, SORTED_ARRAY_SIZE);
+
+    // warm-up iterations
+    for (int i = 0; i < NUM_ITERS; ++i) {
+        fastBinarySearch.upper_bound(query[i]);
+    }
+
     measurer_fast.start();
     for (int i = 0; i < NUM_ITERS; ++i) {
         fast_result[i] = fastBinarySearch.upper_bound(query[i]);
     }
     measurer_fast.stop();
 
-    // TODO: results assertion
+    assertEquals(slow_result, fast_result, NUM_ITERS);
 
     return 0;
 }
